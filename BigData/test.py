c92d78c67efe3364eb56642d3bbd91294ca3f2f4
yingxinlin002 <yingxinlin002@gmail.com>
2025-03-08 17:01:44 -0600

    Delete BigData directory

--- BigData/test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-import pandas as pd
-import csv
-
-with open('tableA.csv', 'r', encoding="utf8") as file:
-    csv_reader = csv.reader(file)
-
-    for line in csv_reader:
-        print(line)
